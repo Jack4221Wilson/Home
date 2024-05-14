c4fd6332d0f94f9de45d36949573740dd08e739b
Jack4221Wilson <jackawilson4221@gmail.com>
2024-05-14 13:41:19 -0500

    Balls are sized on load, font is reactive

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const svgns = 'http://www.w3.org/2000/svg'
 const ballBox = document.querySelector('#backgroundBalls svg')
+const viewHeight = document.querySelector('body').clientHeight
+const viewWidth = document.querySelector('body').clientWidth
+let ballRadius = Math.ceil(parseInt(viewHeight) * 0.3)
+if (viewWidth < viewHeight) {
+    ballRadius = Math.ceil(parseInt(viewWidth) * 0.3)
+}
+console.log(ballRadius)
 
 function createBalls () {
-  const radius = 100
-  let startingX = 100
-  let startingY = 100
+  let startingX = ballRadius
+  let startingY = ballRadius
   for (let i = 0; i < 8; i++) {
     const ball = document.createElementNS(svgns, 'circle')
     ball.setAttribute('cx', `${startingX}`)
     ball.setAttribute('cy', `${startingY}`)
-    ball.setAttribute('r', `${radius}`)
+    ball.setAttribute('r', `${ballRadius}`)
     ball.setAttribute('fill', 'url(#ballGradient)')
     ball.setAttribute('filter', 'url(#displacementFilter)')
     ballBox.appendChild(ball)
-    startingX += 50
-    startingY += 50
+    startingX += 1
+    startingY += 1
   }
 }
 createBalls()
@@ -23,7 +29,7 @@ const ballObj = {
   ele: null,
   vector: [0,0],
   position: [0,0],
-  radius: 100
+  radius: ballRadius
 }
 let ballList = []
 const ballEleList = document.querySelectorAll('circle')
@@ -35,15 +41,14 @@ ballEleList.forEach((ele, i) => {
   ball.position = [ball.ele.cx.baseVal.value, ball.ele.cy.baseVal.value]
   ballList.push(ball)
 })
-console.log(ballList)
 
 function moveBall (ball) {
   
   setInterval(function () {
     ball.position[0] += ball.vector[0]
     ball.position[1] += ball.vector[1]
-    if (ball.position[0] < 100 || ball.position[0] > ballBox.clientWidth - 100) {ball.vector[0] = -ball.vector[0]}
-    if (ball.position[1] < 100 || ball.position[1] > ballBox.clientHeight - 100) {ball.vector[1] = -ball.vector[1]}
+    if (ball.position[0] < ballRadius || ball.position[0] > ballBox.clientWidth - ballRadius) {ball.vector[0] = -ball.vector[0]}
+    if (ball.position[1] < ballRadius || ball.position[1] > ballBox.clientHeight - ballRadius) {ball.vector[1] = -ball.vector[1]}
     ball.ele.setAttribute('cx', `${ball.position[0]}`)
     ball.ele.setAttribute('cy', `${ball.position[1]}`)
   }, fps)
