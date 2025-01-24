c7d5640123a1bbccf3ce3b048ea478cab0c4b924
Jack Wilson <jackawilson4221@gmail.com>
2025-01-23 20:47:04 -0600

    art page gets data from the art object files

--- pages/art/art.js
+++ pages/art/art.js
@@ -1,10 +1,10 @@
 const url = new URL(document.location.href);
 const art_id = parseInt(url.searchParams.get("piece-id"));
 
-let img_ele = document.querySelector('img .display-img');
+let img_ele = document.querySelector('img');
 let header = document.querySelector('h1');
-let info_ele = document.querySelector('p .info');
-let copy_ele = document.querySelector('p .copy');
+let info_ele = document.querySelector('.info');
+let copy_ele = document.querySelector('.copy');
 
 findPiece();
 
@@ -17,21 +17,30 @@ async function getArtList() {
     const art_list = await response.json();
 
     return art_list;
+    /* let art_list = fetch('http://jackawilson.art/pages/art_objects.json', {
+        method: 'GET',
+        mode: "no-cors",
+    })
+    .then((response) => console.log(response));
+    //.then((data) => {return data});
+
+    return art_list;*/
 };
 
 function findPiece() {
     let art = {};
     getArtList()
         .then((art_list) => {
-           art_list.forEach((art_obj) => {
-               if (art_id != art_obj.pieceNum) { return };
-               art = art_obj
+            console.log(art_list);
+            art_list.forEach((art_obj) => {
 
-               console.log(art, img_ele)
-               img_ele.setAttribute('src', `${art.image}`);
-               header.innerHTML = art.header;
-               info_ele.innerHTML = art.info;
-               copy_ele.innerHTML = art.copy;
+                if (art_id != art_obj.pieceNum) { return };
+                art = art_obj
+
+                img_ele.setAttribute('src', `${art.image}`);
+                header.innerHTML = art.header;
+                info_ele.innerHTML = art.info;
+                copy_ele.innerHTML = art.copy;
             });
         });
 }
